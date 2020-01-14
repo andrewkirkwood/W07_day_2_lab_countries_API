5fc4c54fa4010d46e2c0ce2267102dbc85ee550a
Andrew Kirkwood <andrewkirkwood90@gmail.com>
2020-01-14 15:32:49 +0000

    MVP complete, had issue with accessing front-end value

--- src/app.js
+++ src/app.js
@@ -4,7 +4,10 @@ document.addEventListener('DOMContentLoaded', () => {
   new Vue({
     el: '#app',
     data: {
-      countries: []
+      countries: [],
+      index: null,
+      selectedCountry: {},
+      favouriteCountries: []
     },
     mounted: function() {
       this.fetchCountries()
@@ -14,6 +17,14 @@ document.addEventListener('DOMContentLoaded', () => {
         fetch("https://restcountries.eu/rest/v2/all")
         .then(response => response.json())
         .then(countries => this.countries = countries)
+      },
+      selectCountry: function(index) {
+        // const selectedCountry = countries.find(country)
+        this.selectedCountry = this.countries[index]
+      },
+      saveToFavourites: function() {
+        this.favouriteCountries.push(this.countries[this.index])
+        console.log(this.favouriteCountries);
       }
     },
   })
